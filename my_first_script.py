f1b03ae86660686b66e6241355b7daf9304f1eb9
Jugal Deepak Chauhan <43086157+jugal-chauhan@users.noreply.github.com>
2019-01-30 07:58:17 +0530

    new python files uploaded

---
+++ my_first_script.py
@@ -0,0 +1,7 @@
+def welcome_message(name):
+#Prints out a personalised welcome message
+    return "Welcome to this Python script, " + name + "!"
+
+#Call the welcome message function with the input "Nuclei Tech Student" 
+#and print the output
+print(welcome_message("Nuclei Tech Student"))
